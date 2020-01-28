4ae22c2f8baa9f2a3f81969a137a7248cf3ad541
devHoek <60395427+devHoek@users.noreply.github.com>
2020-01-28 16:23:29 +0100

    Code is not clear
    Using + operator to coerce variables to numbers to avoid string concatenation.

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // Using + operator to coerce variables to numbers to avoid string concatenation.
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
