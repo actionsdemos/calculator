76ca24aaf5345a9460b5466cf808c73269e9a49a
Simon Kingaby <skingaby@gmail.com>
2020-03-13 15:42:55 -0500

    Still boned

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,9 +11,9 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
-    'multiply': function(a,b) { return a * b },
+    'multiply': function(a,b) { return a ** b },
     'divide':   function(a,b) { return a / b },
   };
 
