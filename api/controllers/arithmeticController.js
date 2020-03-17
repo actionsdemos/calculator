5ed6cddaeda74b0f3f30fcf8eef55bb301b9deaa
anjukandathil <35814295+anjukandathil@users.noreply.github.com>
2020-03-17 09:03:05 -0700

    code change-1
    code change -1

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -9,7 +9,7 @@ exports.calculate = function(req, res) {
     res.status(400);
     res.json({ error: err.message });
   });
-
+// code change
   var operations = {
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
