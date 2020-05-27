c0a83a2d2b9abf947adcfc641e9f8faf0d3af333
rubio832 <66029626+rubio832@users.noreply.github.com>
2020-05-27 13:55:36 -0500

    Update 3

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    //las variables deben tener el + adelante
+    //las variables deben tener el + adelante !!!!
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
