cab2687194f86ccf9ae678ea708ec5518ede17ca
Goverts <72695373+Goverts@users.noreply.github.com>
2020-10-11 16:44:46 +0200

    Update arithmeticController.js
    Replaced plus signs and added comment,

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,6 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
+    // operation is correct but confusing; using plus operator to coerce variables to number to avoid string concatenation
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
