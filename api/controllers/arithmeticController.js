390c9783cff6cbfb05cc748d54a9712d1b231cdc
ysliu3039 <48102746+ysliu3039@users.noreply.github.com>
2020-11-14 03:46:34 +0000

    added comment
    code is right

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    //Using + operator to coerce variables to numbers to avold string concatention
+    //Using + operator to coerce variables to numbers to avold string concatention  asfdsaf
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
