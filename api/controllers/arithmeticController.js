3ac71ed20efe2448a7afb72ec8e7e369872369e2
Cristian Agudelo <44304934+cristian3165@users.noreply.github.com>
2020-12-09 19:43:56 -0500

    Update addition1

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -15,7 +15,7 @@ exports.calculate = function(req, res) {
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
-  };
+  }; //comenta
 
   // Determine the operation
 
