8405d83ad95f8fea14154c066eb7c82735491a23
miropas <43651000+miropas@users.noreply.github.com>
2021-01-13 13:56:06 +0100

    Fixing the add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -24,7 +24,7 @@ exports.calculate = function(req, res) {
   }
 
   var operation = operations[req.query.operation];
-
+ 
   if (! operation) {
     throw new Error("Invalid operation: " + req.query.operation);
   }
