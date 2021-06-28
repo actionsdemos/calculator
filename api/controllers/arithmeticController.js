8278b5e0403d068b69fa933075dc1b75254674d3
nirmaldevi <86386120+nirmaldevi@users.noreply.github.com>
2021-06-28 09:20:59 -0700

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -29,7 +29,7 @@ exports.calculate = function(req, res) {
     throw new Error("Invalid operation: " + req.query.operation);
   }
 
-  // Validate operands
+  // Validate operands Nirmala
 
   if (! req.query.operand1 ||
       ! req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
