aa67a8ea378398327746658267d55ba8c25d36ed
nnylear67 <57957874+nnylear67@users.noreply.github.com>
2021-07-27 12:42:32 -0700

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -9,7 +9,6 @@ exports.calculate = function(req, res) {
     res.status(400);
     res.json({ error: err.message });
   });
-
   var operations = {
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
