5c2a7b7caf63b0cccf932ee8489a622d88f50bae
vijendra-kumar <vijendra3mca@gmail.com>
2022-03-01 22:00:22 +0000

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,6 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
+    //This is the change
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
