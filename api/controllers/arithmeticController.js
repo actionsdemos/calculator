029fe9ac2566855c9aba05d9cf3ddc2c7a2bed78
StudentJensens <105720939+StudentJensens@users.noreply.github.com>
2022-05-17 06:02:55 -0700

    Update arithmeticController.js
    Modifying the add function 2

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -17,7 +17,7 @@ exports.calculate = function(req, res) {
     'divide':   function(a,b) { return a / b },
   };
 
-  // Determine the operation
+  // Determine the operation  dfd
 
   if (! req.query.operation) {
     throw new Error("Unspecified operation");
