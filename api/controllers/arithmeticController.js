886e56eb7c03769a6be00d6c6cee244fd0398ed8
Henrique Monteiro Vergara <36764823+henriquevergara@users.noreply.github.com>
2022-07-28 19:48:38 -0300

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add': function(a,b) { return +a + +b },
+    'add': function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
