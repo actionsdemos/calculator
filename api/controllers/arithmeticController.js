39c8106712c7802549d0d605345d137fbd5f8ab4
Paris2083 <102171285+parisbrookerPI@users.noreply.github.com>
2022-10-02 18:06:19 +0100

    Modify the add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-exports.calculate = function(req, res) {
-  req.app.use(function(err, req, res, next) {
+exports.calculate = function (req, res) {
+  req.app.use(function (err, req, res, next) {
     if (res.headersSent) {
       return next(err);
     }
@@ -11,35 +11,47 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return +a + +b },
-    'subtract': function(a,b) { return a - b },
-    'multiply': function(a,b) { return a * b },
-    'divide':   function(a,b) { return a / b },
+    add: function (a, b) {
+      return a + b;
+    },
+    subtract: function (a, b) {
+      return a - b;
+    },
+    multiply: function (a, b) {
+      return a * b;
+    },
+    divide: function (a, b) {
+      return a / b;
+    },
   };
 
   // Determine the operation
 
-  if (! req.query.operation) {
+  if (!req.query.operation) {
     throw new Error("Unspecified operation");
   }
 
   var operation = operations[req.query.operation];
 
-  if (! operation) {
+  if (!operation) {
     throw new Error("Invalid operation: " + req.query.operation);
   }
 
   // Validate operands
 
-  if (! req.query.operand1 ||
-      ! req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
+  if (
+    !req.query.operand1 ||
+    !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+    req.query.operand1.replace(/[-0-9e]/g, "").length > 1
+  ) {
     throw new Error("Invalid operand1: " + req.query.operand1);
   }
 
-  if (! req.query.operand2 ||
-      ! req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand2.replace(/[-0-9e]/g, '').length > 1) {
+  if (
+    !req.query.operand2 ||
+    !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+    req.query.operand2.replace(/[-0-9e]/g, "").length > 1
+  ) {
     throw new Error("Invalid operand2: " + req.query.operand2);
   }
 
