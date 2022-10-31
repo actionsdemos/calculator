dfc56d0cc89409d711b5fcee979196084836684a
BGA-DXC <56201781+BGA-DXC@users.noreply.github.com>
2022-10-31 15:10:04 +0100

    Fixed-Again
    Fixed-Again

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -13,7 +13,8 @@ exports.calculate = function(req, res) {
   var operations = {
   // Using + operator to type cast variables as integers in order to prevent string concatenation
   // Updating Again with an error
-    'add':      function(a,b) { return a + b },
+  // Fixed Again 
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
