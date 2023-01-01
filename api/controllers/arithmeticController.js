80b5eb2fca42179447385600972df7da8a91b255
jgalley2017 <76413857+jgalley2017@users.noreply.github.com>
2023-01-01 10:32:51 +0000

    Fixing the add function
    "+a + +b" from "a + b"

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -10,10 +10,9 @@ exports.calculate = function(req, res) {
     res.json({ error: err.message });
   });
 
-  //changed add function to 'return a + b' from 'return +a + +b'
-  // added addition-cleanup branch
+// Using + operator to type cast variables as integers in order to prevent string concatenation.
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
