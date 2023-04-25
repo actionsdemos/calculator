9641d6f29d75707b80eb3338b0d97100b42ceefd
mcantu <12145912+mcantu@users.noreply.github.com>
2023-04-25 11:51:40 -0500

    adding stuff

---
+++ hello.js
@@ -0,0 +1,9 @@
+let auth_id = 'AKIAVPTPVSVXOU6WIP'
+let auth_token = 'QAJk/WpNmQk+tJ8r3+boU2qt0vV6AygTMlFdW'
+
+let auth_id = 'AKIAVPTPVSVXOU6WIP'
+
+
+// Path: secrets.js
+
+AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2Q
