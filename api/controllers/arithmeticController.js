2ccf6d885483c1c7a36efa90607d193bd1d2699e
Vik <vik.malhotra@databricks.com>
2023-08-04 13:52:32 -0400

    Added comment to explain adddition
    code  is efficient, but confusing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // Uisng + operator to coerce varibales to numb ers to avoid string concatenation
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
