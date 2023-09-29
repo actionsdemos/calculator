752cf7b3cfa59a6eafdba782e96e94436f4371e7
tiduzz85 <126408473+tiduzz85@users.noreply.github.com>
2023-09-29 12:09:05 +0800

    Update arithmeticController.js
    Luckily read back the in the instruction.

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,8 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return },
-    'subtract': function(a,b) { return +a + +b },
+    'add':      function(a,b) { return +a + +b },
+    'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
   };
