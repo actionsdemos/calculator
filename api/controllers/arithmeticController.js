98c1f1a2501dbd05d4576885c1173e7fe7d756e2
mc230923497 <145974336+mc230923497@users.noreply.github.com>
2023-11-28 21:05:56 +0800

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,7 @@ exports.calculate = function(req, res) {
 
   var operations = {
     // Using + operator to coerce variables to numbers to avoid string concatenation.
-    'add':      function(a,b) { return +a + +b },
+    'add':      function(a,b) { return +a +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
